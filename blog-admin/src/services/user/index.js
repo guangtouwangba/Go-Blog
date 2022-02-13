cb4dcb6340a6e5a7aca67d42b015e56474edcc27
Siqiu Chen <819110812@qq.com>
2022-02-13 19:06:03 +0800

    修改前端登陆判断

--- blog-admin/src/services/user/index.js
+++ blog-admin/src/services/user/index.js
@@ -2,6 +2,7 @@ import { request } from 'umi';
 
 /** 登录接口 POST /api/login/account */
 export async function login(data, options) {
+  console.log(data)
   return request('/api/login/account', {
     method: 'POST',
     headers: {
