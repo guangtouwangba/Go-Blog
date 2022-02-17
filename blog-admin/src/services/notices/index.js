aea08fca3e58dc81d789dcb13f664f81a7f23002
Siqiu Chen <819110812@qq.com>
2022-02-17 15:17:45 +0800

    删除无用目录

--- blog-admin/src/services/notices/index.js
+++
@@ -1,9 +0,0 @@
-import { request } from 'umi';
-
-/** 获取消息列表 GET /api/notices */
-export async function getNotices(options) {
-  return request('/api/notices', {
-    method: 'GET',
-    ...(options || {}),
-  });
-}
