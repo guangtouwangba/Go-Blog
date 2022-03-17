f5af954e4a1f03ff8bcdd14addabdd1e61c099af
Chen, Siqiu (External) <Siqiu.Chen@dentsplysirona.com>
2022-03-17 08:40:20 +0800

    add pre commit hook

--- internal/adapter/inbound/rest/controller/auth_controller.go
+++ internal/adapter/inbound/rest/controller/auth_controller.go
@@ -1,12 +1,7 @@
 package controller
 
 import (
-	"Go-Blog/internal/app/usecase"
-	"Go-Blog/internal/domain/dto/response"
 	"Go-Blog/internal/service"
-	"github.com/gin-gonic/gin"
-	"log"
-	"net/http"
 )
 
 var jwtService = service.JWTService{}
@@ -15,21 +10,21 @@ type AuthController struct {
 	BaseController
 }
 
-func (a *AuthController) GetToken(c *gin.Context) {
-	username := c.Param("name")
-	log.Println(username)
-	usecases := usecase.UserUseCase{
-		UserRepository: &service.UserService{},
-	}
-	user, err := usecases.GetUserByUserName(username)
-	if err != nil {
-		response.Error(http.StatusInternalServerError, c)
-		return
-	}
-
-	log.Println(user)
-}
-
-func (a *AuthController) generateToken(username string, password string) (string, error) {
-	return jwtService.GenerateJWTToken(username, password)
-}
+//func (a *AuthController) GetToken(c *gin.Context) {
+//	username := c.Param("name")
+//	log.Println(username)
+//	usecases := usecase.UserUseCase{
+//		UserRepository: &service.UserService{},
+//	}
+//	user, err := usecases.GetUserByUserName(username)
+//	if err != nil {
+//		response.Error(http.StatusInternalServerError, c)
+//		return
+//	}
+//
+//	log.Println(user)
+//}
+//
+//func (a *AuthController) generateToken(username string, password string) (string, error) {
+//	return jwtService.GenerateJWTToken(username, password)
+//}
